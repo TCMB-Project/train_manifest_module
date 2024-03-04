a5cd437e09696deaf74265d8b6955d11a4f687dc
幻想郷のけんけん <51988985+gensokyo-kenken@users.noreply.github.com>
2024-03-04 15:33:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import * as manifest from "./manifest";
 let overworld = world.getDimension('overworld');
 let scriptevent_recevie = system.afterEvents.scriptEventReceive.subscribe((event) => {
   if (event.id == 'tcmb_minecart_engine:initalized') {
-    overworld.runCommandAsync('scriptevent tcmb_minecart_engine:regist_tcmanifest ' + JSON.stringify(manifest.default));
+    overworld.runCommandAsync('scriptevent tcmb_minecart_engine:regist_manifest ' + JSON.stringify(manifest.default));
     system.afterEvents.scriptEventReceive.unsubscribe(scriptevent_recevie);
   }
 }, { namespaces: ['tcmb_minecart_engine'] });
